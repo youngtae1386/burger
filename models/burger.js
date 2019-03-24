d93578bf6b46207e94c042f35e4ed503cb3c26c9
Young Cho <youngtae1386@gmail.com>
2019-03-23 23:36:33 -0500

    updating jawdb

--- models/burger.js
+++ models/burger.js
@@ -2,19 +2,19 @@
 var orm = require("../config/orm.js");
 
 var burger = {
-    all: function(cb) {
+    selectAll: function(cb) {
         orm.all("burgers_db.burgers", function(res) {
             cb(res);
         });
     },
-    create: function(name, cb) {
+    addBurger: function(name, cb) {
         orm.create("burgers", [
             "burger_name", "devoured"
         ], [
             name, false
         ], cb);
     },
-    update: function(id, cb) {
+    updateBurger: function(id, cb) {
         var condition = "id=" + id;
         orm.update("burgers", {
             devoured: true
