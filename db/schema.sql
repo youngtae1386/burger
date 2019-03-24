4bed2ca3797caa6d6cb1faae7aa1e63c74233329
Young Cho <youngtae1386@gmail.com>
2019-03-23 22:56:56 -0500

    updating jawdb

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,8 @@
-USE q8x7rbdt2fa9hup2;
-CREATE DATABASE IF NOT EXISTS burgers_db;
-USE burgers_db;
-
--- # If the table already exists, remove it before trying to create the table again
-DROP TABLE IF EXISTS burgers;
-
--- # Create the burgers table
-CREATE TABLE burgers (
-id int NOT NULL AUTO_INCREMENT,
-burger_name varchar(255) NOT NULL,
-devoured BOOL DEFAULT false,
-PRIMARY KEY (id));
+USE w703gxk6knqyzlt7;
+CREATE TABLE `burgers` (
+  `id` bigint(60) NOT NULL AUTO_INCREMENT,
+  `burger_name` varchar(100) NOT NULL,
+  `devoured` boolean,
+  date TIMESTAMP not null DEFAULT current_timestamp,
+  PRIMARY KEY (`id`)
+);
