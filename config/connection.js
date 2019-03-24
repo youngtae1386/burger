12f97f84dd034b44e55d86c2abc066d99d5e3df3
Young Cho <youngtae1386@gmail.com>
2019-03-23 22:47:12 -0500

    updating jawdb

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,44 @@
 require('dotenv').config()
-var keys = require("./keys.js");
+//var keys = require("./keys.js");
 var mysql = require("mysql");
 
-var connection;
-if(process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {connection = mysql.createConnection(keys.AccessMysql)};
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+// var connection;
+// if(process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {connection = mysql.createConnection(keys.AccessMysql)};
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
-module.exports = connection;
+// module.exports = connection;
+
+let mysql = require('mysql');
+
+let connection;
+
+if (process.env.JAWSDB_URL) {
+  console.log(process.env.JAWSDB_URL)
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection.env({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'Qwer!234',
+    database: 'burgers_db'
+  });
+}
+
+  connection.connect((err) => {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+    console.log('connected as id ' + connection.threadId);
+  });
+
+  module.exports = connection;
--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,4 @@
-GRANT SELECT ON burgers_db.* TO 'rsuwcyvur3w4fbx4'@'localhost';
+USE q8x7rbdt2fa9hup2;
 CREATE DATABASE IF NOT EXISTS burgers_db;
 USE burgers_db;
 
@@ -10,4 +10,4 @@ CREATE TABLE burgers (
 id int NOT NULL AUTO_INCREMENT,
 burger_name varchar(255) NOT NULL,
 devoured BOOL DEFAULT false,
-PRIMARY KEY (id)).
+PRIMARY KEY (id));
